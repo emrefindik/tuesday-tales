3dcdbfcb65a67a8f4dbf8bef48f565a2021e4f01
mercurian-h <nicky.hsj@gmail.com>
2017-02-21 16:44:54 -0500

    Add Spatial Api Examples

---
+++ APIExamples/SpatialAPI/AnotherScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherScript : MonoBehaviour {
+
+    SpatialClient2 spatialClient;
+    GoogleMap gmClient;
+    // Use this for initialization
+    void Start () {
+        spatialClient = GetComponent<SpatialClient2>();
+        gmClient = GetComponent<GoogleMap>();
+        if (spatialClient != null)
+        {
+            //spatialClient.GetProjectInfo("588fb546604ae700118697c5");
+            //StartCoroutine(spatialClient.GetProjectInfo("588fb546604ae700118697c5"));
+
+            //StartCoroutine(spatialClient.CreateMarker(spatialClient.projectID, 40.433, -79.964, "test", "description", null));
+
+            StartCoroutine(spatialClient.GetMarkersByProject(spatialClient.projectID));
+            
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (spatialClient.ready)
+        {
+            gmClient.swapMarkers(spatialClient.markers);
+            spatialClient.ready = false;
+        }
+    }
+}
