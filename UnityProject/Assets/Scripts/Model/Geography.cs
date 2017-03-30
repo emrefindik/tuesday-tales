d9169a9a9566e1a9321982bd31d743e09dd7b877
mercurian-h <Mercurian@Nickys-MBP.wv.cc.cmu.edu>
2017-03-30 10:17:13 -0400

    Add Geography and addmarkers.html

---
+++ UnityProject/Assets/Scripts/Model/Geography.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LocationCoord
+{
+	public double lat;
+	public double lon;
+}
+
+[System.Serializable]
+public class Path
+{
+	List<LocationCoord> locations;
+}
+
+public class Geography {
+	
+	const double EARTH_RADIUS = 6371d;
+	static public bool withinDistance(LocationCoord loc1, LocationCoord loc2, double radius){
+		if (radius > getDistanceFromLatLonInM (loc1.lat, loc1.lon, loc2.lat, loc2.lon))
+			return true;
+
+		return false;
+	}
+
+	static double getDistanceFromLatLonInM(double lat1, double lon1, double lat2, double lon2){
+		double dLat = deg2rad(lat2-lat1);  // deg2rad below
+		double dLon = deg2rad(lon2-lon1); 
+		double a = 
+			System.Math.Sin(dLat/2) * System.Math.Sin(dLat/2) +
+			System.Math.Cos(deg2rad(lat1)) * System.Math.Cos(deg2rad(lat2)) * 
+			System.Math.Sin(dLon/2) * System.Math.Sin(dLon/2)
+			; 
+		double c = 2 * System.Math.Atan2(System.Math.Sqrt(a), System.Math.Sqrt(1-a)); 
+		double d = EARTH_RADIUS * c; // Distance in km
+		return d * 1000;	// Distance in m
+	}
+
+	static double deg2rad(double deg) {
+		return deg * (Mathf.PI / 180);
+	}
+}
