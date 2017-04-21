f73bbad7e19147184a8b01d22dd53f0243947f2e
mercurian-h <Mercurian@Nickys-MBP.wv.cc.cmu.edu>
2017-04-21 16:47:25 -0400

    Add sound

--- UnityProject/Assets/Scripts/Controller/PhoneImageController.cs
+++ UnityProject/Assets/Scripts/Controller/PhoneImageController.cs
@@ -87,6 +87,7 @@ public class PhoneImageController : MonoBehaviour {
 			EggSelfieModel.SetActive (true);
 			GameObject Egg = GameObject.Find ("Egg");
 			Egg.GetComponent<SpriteRenderer> ().sprite = mainController.selectedEgg.Sprite;
+			Egg.transform.localScale = new Vector3 (873.0f / mainController.selectedEgg.Sprite.texture.width, 878.0f / mainController.selectedEgg.Sprite.texture.height);
 			break;
 		case CameraMode.Kaiju:
 			KaijuSelfieModel.SetActive (true);
