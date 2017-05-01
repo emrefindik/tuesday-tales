5dedc55f7403005d48098909ad2dafe952df04b6
mercurian-h <Mercurian@Nickys-MBP.wv.cc.cmu.edu>
2017-05-01 15:37:47 -0400

    Make Timer Goes Down (half every time)

--- UnityProject/Assets/Scripts/Controller/SpatialClient2.cs
+++ UnityProject/Assets/Scripts/Controller/SpatialClient2.cs
@@ -1347,7 +1347,7 @@ public class UserMetadata// : ISerializationCallbackReceiver
         if (streakTimerStart <= 0 || streakTimerStart == NO_STREAK)
             streakTimerStart = INITIAL_RAMPAGE_INTERVAL;
         else
-            streakTimerStart *= 2;
+            streakTimerStart *= 0.5;
         scoreMultiplier++;
         streakMarkers.addMarkerId(newMarkerId);
     }
